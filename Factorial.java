f5da35147ea81c09de6ea7c30c35f4801f3d33ad
guglothkumari <guglothkumari@gmail.com>
2026-08-13 16:47:52 +0530

    Add factorial program and tests

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+class Factorial{
+    public static long compute(int n){
+        if(n<0){
+            throw new IllegalArgumentException("Input must be non-negative");
+        }
+        long result=1;
+
+        for(int i=2;i<=n;i++){
+            result*=i;
+        }
+        return result;
+    }
+}

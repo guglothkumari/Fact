6de19f5589bcdf3200d5c23bdb3c3644dd5e4db8
guglothkumari <guglothkumari@gmail.com>
2026-08-13 16:57:22 +0530

    Refactor Factorial class for improved readability

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,20 @@
-class Factorial{
-    public static long compute(int n){
-        if(n<0){
+class Factorial {
+
+    public static long compute(int n) {
+        if (n < 0) {
             throw new IllegalArgumentException("Input must be non-negative");
         }
-        long result=1;
 
-        for(int i=2;i<=n;i++){
-            result*=i;
+        long result = 1;
+
+        for (int i = 2; i <= n; i++) {
+            result *= i;
         }
+
         return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of 5 = " + compute(5));
     }
 }

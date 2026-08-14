856b422f47905ae951e830ba63a69f937131d7a9
guglothkumari <guglothkumari@gmail.com>
2026-08-14 14:27:32 +0530

    Update main method to compute factorial of 7

--- Factorial.java
+++ Factorial.java
@@ -15,6 +15,6 @@ class Factorial {
     }
 
     public static void main(String[] args) {
-        System.out.println("Factorial of 5 = " + compute(5));
+        System.out.println("Factorial of 7 = " + compute(7));
     }
 }
